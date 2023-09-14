234ede8a2928f287dcc999310df0887f31f1a7b7
harsha619 <harshavardhan.macchetti@zemosolabs.com>
2023-09-14 13:53:30 +0530

    moved initialisation into constructor

--- christmas-lights/src/main/java/com/coderetreat/christmasLightsKata/ChristmasLightsImpl.java
+++ christmas-lights/src/main/java/com/coderetreat/christmasLightsKata/ChristmasLightsImpl.java
@@ -3,15 +3,15 @@ package com.coderetreat.christmasLightsKata;
 public class ChristmasLightsImpl implements IChristmasLights {
     private int[][] lights;
 
-    ILightingOperations lightingOperations = new LightOperations() ;
+    ILightingOperations lightingOperations;
+
     public ChristmasLightsImpl(int totalNoOfLights) {
         lights = new int[totalNoOfLights][totalNoOfLights];
+        lightingOperations = new LightOperations();
     }
 
-
-
     @Override
-    public  void executeInstructions(String[] instructions){
+    public void executeInstructions(String[] instructions) {
         for (String instruction : instructions) {
             String[] parts = instruction.split(" ");
             if (parts[0].equals("turn")) {
@@ -20,9 +20,9 @@ public class ChristmasLightsImpl implements IChristmasLights {
                 int endX = Integer.parseInt(parts[4].split(",")[0]);
                 int endY = Integer.parseInt(parts[4].split(",")[1]);
                 if (parts[1].equals("on")) {
-                    lights=lightingOperations.lightOn(lights,startX, startY, endX, endY);
+                    lights = lightingOperations.lightOn(lights, startX, startY, endX, endY);
                 } else if (parts[1].equals("off")) {
-                   lights= lightingOperations.lightOff(lights,startX, startY, endX, endY);
+                    lights = lightingOperations.lightOff(lights, startX, startY, endX, endY);
                 }
             } else if (parts[0].equals("toggle")) {
                 int startX = Integer.parseInt(parts[1].split(",")[0]);
@@ -30,7 +30,7 @@ public class ChristmasLightsImpl implements IChristmasLights {
                 int endX = Integer.parseInt(parts[3].split(",")[0]);
                 int endY = Integer.parseInt(parts[3].split(",")[1]);
 
-               lights= lightingOperations.toggle(lights,startX, startY, endX, endY);
+                lights = lightingOperations.toggle(lights, startX, startY, endX, endY);
             }
 
         }
