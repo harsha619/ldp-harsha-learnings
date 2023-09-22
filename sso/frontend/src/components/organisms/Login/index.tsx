cefd7680587095d4bae33f7d9ad81cdeff616998
harsha619 <harshavardhan.macchetti@zemosolabs.com>
2023-09-22 10:06:15 +0530

    modifed login page

--- sso/frontend/src/components/organisms/Login/index.tsx
+++ sso/frontend/src/components/organisms/Login/index.tsx
@@ -73,6 +73,7 @@ export const Login = () => {
                 },
               });
             }}
+            variant="contained"
           >
             Google login
           </Button>
--- sso/frontend/src/index.tsx
+++ sso/frontend/src/index.tsx
@@ -1,5 +1,17 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Auth0Provider
+    domain={process.env.DOMAIN!}
+    clientId={process.env.CLIENTID!}
+    authorizationParams={{
+      redirect_uri: 'http://localhost:8080/dashboard',
+    }}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+);
