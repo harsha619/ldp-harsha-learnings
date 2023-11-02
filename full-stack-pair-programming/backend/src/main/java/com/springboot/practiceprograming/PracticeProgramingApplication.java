34d0b8f398179abbe4ed2dc736974bfba940da81
harsha619 <harshavardhan.macchetti@zemosolabs.com>
2023-11-02 17:31:57 +0530

    feat: created backed for pair programming

---
+++ full-stack-pair-programming/backend/src/main/java/com/springboot/practiceprograming/PracticeProgramingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.practiceprograming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeProgramingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeProgramingApplication.class, args);
+	}
+
+}
---
+++ full-stack-pair-programming/backend/src/test/java/com/springboot/practiceprograming/PracticeProgramingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.practiceprograming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProgramingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
